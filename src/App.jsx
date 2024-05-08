64d9bc2664e3522565492afb56db0aa8401989ea
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-05-08 10:58:04 -0400

    feat(web):add application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import "./App.css";
+
 function App() {
   return (
     <>
-      <article></article>
+      <main className="main">
+        <article className="generation">
+          <h2 className="title">Random Color Generation</h2>
+          <section className="color"></section>
+          <section className="generation__section">
+            <label className="label">
+              Select Color:
+              <input type="color" name="" className="input" />
+            </label>
+            <button>Save</button>
+          </section>
+          <button className="button">Random</button>
+        </article>
+
+        <article>
+          <h2>Colors Saves</h2>
+          <section>
+            <section>
+              <span>color</span>
+              <p>#1f1234</p>
+              <button>x</button>
+            </section>
+          </section>
+        </article>
+      </main>
     </>
   );
 }
