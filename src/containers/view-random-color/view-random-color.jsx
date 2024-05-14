c0fdb437bf5e83c86c1472b1b2cc30cea3db8f7a
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-05-13 20:16:15 -0400

    refactor(web):change containers folder

---
+++ src/containers/view-random-color/view-random-color.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useColorsContext } from "../../contexts/colors-context";
+import Title from "../../components/title/title";
+import Button from "../../components/button/button";
+import ButtonSaved from "../../components/button-saved/button-saved";
+import "./styles.css";
+
+function ViewRandomColor() {
+  // useState
+  let { hexadecimal } = useColorsContext();
+
+  // Action function
+  let {
+    handlerClickRandomColor,
+    handlerChangeInputColor,
+    handlerClickSaveColor,
+  } = useColorsContext();
+
+  useEffect(() => {
+    handlerClickRandomColor();
+  }, []);
+
+  return (
+    <article className="random-color">
+      <Title text="Random Color Generation" />
+      <input
+        type="color"
+        className="select-color"
+        id="id-select-color"
+        onChange={handlerChangeInputColor}
+      />
+      <section className="random-color__select-color">
+        <section className="random-color__change-color">
+          <p>Change color:</p>
+          <label htmlFor="id-select-color" className="show-color">
+            {hexadecimal}
+          </label>
+        </section>
+        <ButtonSaved onClick={handlerClickSaveColor} />
+      </section>
+
+      <Button onClick={handlerClickRandomColor} text="Random color" />
+    </article>
+  );
+}
+
+export default ViewRandomColor;
