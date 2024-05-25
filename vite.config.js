353ed95c9a071cdeaeccc277c179f9858431ada8
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-05-25 19:33:03 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://BryanGrandon.github.io/Random-Color-Generation/",
+  base: "Random-Color-Generation/",
 });
